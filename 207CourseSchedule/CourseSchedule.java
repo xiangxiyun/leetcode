75d7ebf97b674bf67e61b05f83a875fbc6483a6c
xiangxiyun <xiangxy3@gmail.com>
2018-02-06 22:13:43 -0500

    207 update

--- 207CourseSchedule/CourseSchedule.java
+++ 207CourseSchedule/CourseSchedule.java
@@ -5,11 +5,11 @@ public class CourseSchedule{
 		int numCourses = 2;
 		int[][] prerequisites = {{1,0}};
 
-		boolean res = canFinish(numCourses, prerequisites);
+		boolean res = canFinish_bfs(numCourses, prerequisites);
 		System.out.println(res);
 	
 	}
-	private static boolean canFinish(int numCourses, int[][] prerequisites){
+	private static boolean canFinish_dfs(int numCourses, int[][] prerequisites){
 		//building graph
 		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 		for(int i = 0; i < numCourses; i++){
@@ -22,10 +22,10 @@ public class CourseSchedule{
 
 
 		//topological sort
-		int[] visited = new int[numCourses];
+		boolean[] visited = new boolean[numCourses];
 		boolean res = true;
 		for(int i = 0; i < numCourses; i++){
-			if(visited[i] != 2 && !dfs(visited, graph, i))
+			if(!dfs(visited, graph, i))
 				res = false;
 		}
 
@@ -33,23 +33,64 @@ public class CourseSchedule{
 		
 	}
 
-	private static boolean dfs(int[] visited, ArrayList<ArrayList<Integer>> graph, int c){
-		if(visited[c] == 1)
+	private static boolean dfs(boolean[] visited, ArrayList<ArrayList<Integer>> graph, int c){
+		if(visited[c] == true)
 			return false;
 
-		visited[c] = 1;
+		visited[c] = true;
 
 		ArrayList<Integer> courses = graph.get(c);
 
 		for(int i = 0; i < courses.size(); i++){
 			int cid = courses.get(i);
-			if(visited[cid]!=2 && !dfs(visited, graph, cid))
+			if(!dfs(visited, graph, cid))
 				return false;
 		}
 
-		visited[c] = 2;
+		visited[c] = false;
 		return true;
 	}
 
+	private static boolean canFinish_bfs(int numCourses, int[][] prerequisites){
+		ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+		for(int i = 0; i < numCourses; i++){
+			graph.add(new ArrayList<Integer>());
+		}
+
+		int[] degree = new int[numCourses];
+
+		for(int i = 0; i < prerequisites.length; i++){
+			degree[prerequisites[i][1]]++;
+			graph.get(prerequisites[i][0]).add(prerequisites[i][1]);
+		}
+
+		ArrayDeque<Integer> queue = new ArrayDeque<>();
+		int count = 0;
+
+		for(int i = 0; i < degree.length; i++)
+			if(degree[i] == 0){
+				queue.add(i);
+				count++;
+			}
+
+
+		while(!queue.isEmpty()){
+			int node = queue.poll();
+			ArrayList<Integer> nlist = graph.get(node);
+			for(int i = 0; i < nlist.size(); i++){
+				int nid = nlist.get(i);
+				degree[nid]--;
+				if(degree[nid] == 0){
+					queue.add(nid);
+					count++;
+				}
+			}
+		}
+
+		if(count != numCourses)
+			return false;
+		else
+			return true;
+	}
 
 }
